3394b16f8ae7c32ca24a46c65ddce35f1d4c8cda
tomazz <tomaz.zagar@gis.si>
2020-01-10 13:07:26 +0100

    install

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 pip install --upgrade pip
+
+apt-get update
+apt-get install gdal-bin python-gdal python3-gdal -y
+pip install GDAL
+
 pip install matplotlib
 pip install scipy==1.2.0
 pip install Pillow
